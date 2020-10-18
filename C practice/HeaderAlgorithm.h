645ac775297c04b636e60102c54399b82c82d9c3
rafis67 <rafisiddique67@gmail.com>
2020-10-18 20:41:48 +0600

    Sum of Even Number

--- C practice/HeaderAlgorithm.h
+++ C practice/HeaderAlgorithm.h
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
 void sum_of_even_number() {
-    int n =1, sum = 0;
-    int arra[100];
-    for(int i=0; i<100; i++){
-        arra[i] = i+1;
-    }
-    for(int i=1; i<100; i+=2){
-        sum+=arra[i];
+    int sum = 0;
+    for(int i=2; i<=100; i+=2){
+        sum+=i;
     }
     printf("%d",sum);
 }
