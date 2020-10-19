c3da95909c3173a7de8427301a9bf01fe2bc8178
rafis67 <rafisiddique67@gmail.com>
2020-10-19 15:24:47 +0600

    Program 4

--- C practice/ComputerMemory.c
+++ C practice/ComputerMemory.c
@@ -1,5 +1,5 @@
 #include "HeaderMemory.h"
 
 void main() {
-    program_3();
+    program_4();
 }
--- C practice/HeaderMemory.h
+++ C practice/HeaderMemory.h
@@ -26,3 +26,16 @@ void program_3() {
         printf("0");
     }
 }
+
+void program_4() {
+    char ch1 = 'A', ch2 = 'B';
+    int n1 = 100, n2 = 100000;
+    printf("Value of ch1 = %c,\t",ch1);
+    printf("Address of ch1 = %p\n",&ch1);
+    printf("Value of ch2 = %d,\t",ch2);
+    printf("Address of ch2 = %p\n",&ch2);
+    printf("Value of n1 = %d,\t",n1);
+    printf("Address of n1 = %p\n",n1);
+    printf("Value of n2 = %d,\t",n2);
+    printf("Address of n2 = %p\n",n2);
+}
