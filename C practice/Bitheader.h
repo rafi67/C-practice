1e96f88dcf487ab45d3dee54a522493fbbc034cc
rafis67 <rafisiddique67@gmail.com>
2020-11-07 18:35:06 +0600

    Bitwise Operation Program 4

--- C practice/Bitheader.h
+++ C practice/Bitheader.h
@@ -39,3 +39,9 @@ void program_3() {
         printf("Result is %d\n",m);
     }
 }
+
+void program_4() {
+    int n1 = 5, n2 = 6, n3;
+    n3 = n1 | n2;
+    printf("%d | %d = %d\n", n1, n2, n3);
+}
