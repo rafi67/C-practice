c917afae61c269aeeec60fd52c91388ee8301510
rafis67 <rafisiddique67@gmail.com>
2020-11-11 20:25:00 +0600

    Structure Program 2

--- C practice/HeaderStructure.h
+++ C practice/HeaderStructure.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 void program_1() {
     struct student {
@@ -8,6 +9,18 @@ void program_1() {
     struct student one;
     one.id = 1;
     one.name = "Tahmid Rafi";
+    printf("ID: %d\n", one.id);
+    printf("Name: %s\n", one.name);
+}
+
+void program_2() {
+    struct student {
+        int id;
+        char name[40];
+    };
+    struct student one;
+    one.id = 2;
+    strcpy(one.name, "Tahmid Rafi");
     printf("ID: %d\n", one.id);
     printf("Name: %s\n", one.name);
 }
--- C practice/Structure.c
+++ C practice/Structure.c
@@ -1,5 +1,5 @@
 #include "HeaderStructure.h"
 
 void main() {
-    program_1();
+    program_2();
 }
