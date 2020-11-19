ee39d85ffa341262f550f3510998160e5fce8767
rafis67 <rafisiddique67@gmail.com>
2020-11-19 17:10:20 +0600

    Pointer 2 Program 2

--- C practice/HeaderPointer2.h
+++ C practice/HeaderPointer2.h
@@ -16,3 +16,20 @@ void program_1() {
         printf("%d\n", marks[i]);
     }
 }
+
+void program_2() {
+    int *marks;
+    int i, n;
+    printf("Enter the number of student: ");
+    scanf("%d", &n);
+    marks = (int *) malloc(sizeof(int) *n);
+    printf("Enter the marks for each student:\n");
+    for(i=0; i<n; i++){
+        scanf("%d", &marks[i]);
+    }
+    printf("Now here you can see the values:\n");
+    for(i=0; i<n; i++){
+        printf("%d\n", marks[i]);
+    }
+    free(marks);
+}
