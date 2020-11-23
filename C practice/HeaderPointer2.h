4dedba611d822ee6ecce2fdcaeed004b26cbefee
rafis67 <rafisiddique67@gmail.com>
2020-11-23 19:36:40 +0600

    Pointer2 Program 7

--- C practice/HeaderPointer2.h
+++ C practice/HeaderPointer2.h
@@ -126,3 +126,24 @@ void program_6() {
     printf("*(p+5) = %d\n", *(p+5));
     printf("*(p+6) = %d\n", *(p+6));
 }
+
+void program_7() {
+    char *p, a = 'F';
+    int *q, b = 10;
+    double *r, c = 302.64;
+    p = &a;
+    q = &b;
+    r = &c;
+    printf("Char size : %d bytes\n", sizeof(char));
+    printf("p = %p\n", p);
+    printf("p+1 = %p\n", p+1);
+    printf("p+2 = %p\n", p+2);
+    printf("Int size : %d bytes\n", sizeof(int));
+    printf("q = %p\n", q);
+    printf("q+1 = %p\n", q+1);
+    printf("q+2 = %p\n", q+2);
+    printf("Double size : %d bytes\n", sizeof(double));
+    printf("r = %p\n", r);
+    printf("r+1 = %p\n", r+1);
+    printf("r+2 = %p\n", r+2);
+}
