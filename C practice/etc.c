657f51614574ba1100a05eaf6de151ad0ea6ad01
rafis67 <rafisiddique67@gmail.com>
2020-12-10 21:58:05 +0600

    Command line Arguments Program 2

--- C practice/etc.c
+++ C practice/etc.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 
 void main(int argc, char *argv[]) {
-    int n1, n2, sum;
-    n1 = atoi(argv[1]);
-    n2 = atoi(argv[2]);
-    sum = n1 + n2;
-    printf("%d + %d = %d\n", n1, n2, sum);
+    printf("Argument count : %d\n", argc);
+    printf("First argument : %s\n", argv[0]);
 }
